466f3e2f91e44c686a6c329af6cf03fa75e297cd
Evahello123 <32111514+Evahello123@users.noreply.github.com>
2017-11-28 19:48:01 -0600

    1.0

--- msg_client.py
+++ msg_client.py
@@ -5,12 +5,12 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Server address
-addr = input("Enter the target IP address: ")
-port = int(input("Enter the target port: "))
+addr = input("Enter the target IP address: 192.17.96.7")
+port = int(input("Enter the target port: 10000"))
 server_address = (addr, port)
 
 # Message to send
-message = bytes(input("Enter your NetID: "), "utf-8")
+message = bytes(input("Enter your NetID: zihewu2"), "utf-8")
 
 try:
     # Send data
